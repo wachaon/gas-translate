32562fef5d18bb6e06553a3345c4217297eac7f2
wachaon <wachaon@gmail.com>
2020-05-17 15:22:41 +0900

    Add file

---
+++ src/gas-translate.js
@@ -0,0 +1,13 @@
+const request = require( 'httprequest' )
+
+const replace = function translate_replace( html ) {
+    return html.replace( /(<\/)( )([A-z]+>)/g, '$1$3')
+}
+
+const translate = function translate_translate ( api, source = null, target = 'en', text, contentType ) {
+    const body = unescape(request( 'get', api, { params: { text, source, target, contentType } } ))
+    if ( contentType === 'html' ) return replace(  body )
+    return body
+}
+
+module.exports = translate
