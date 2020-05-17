49c84b9aeb0efda485848aba1c4a753f233b60c0
wachaon <wachaon@gmail.com>
2020-05-17 15:22:09 +0900

    change

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const request = require( 'httprequest' )
-
-const replace = function translate_replace( html ) {
-    return html.replace( /(<\/)( )([A-z]+>)/g, '$1$3')
-}
-
-const translate = function translate_translate ( api, source = null, target = 'en', text, contentType ) {
-    const body = unescape(request( 'get', api, { params: { text, source, target, contentType } } ))
-    if ( contentType === 'html' ) return replace(  body )
-    return body
-}
-
-module.exports = translate
+module.exports = require('src/gas-translate')
