aad4f1b87dcfc0f512474045b86b60d278cfc598
wachaon@gmail.com <wachaon@gmail.com>
2022-11-27 08:37:27 +0900

    Bundle with the new packaging method

--- src/gas-translate.js
+++ src/gas-translate.js
@@ -1,12 +1,12 @@
-const request = require( 'httprequest' )
+const request = require('httprequest')
 
-const replace = function translate_replace( html ) {
-    return html.replace( /(<\/)( )([A-z]+>)/g, '$1$3')
+const replace = function translate_replace(html) {
+    return html.replace(/(<\/)( )([A-z]+>)/g, '$1$3')
 }
 
-const translate = function translate_translate ( api, source = null, target = 'en', text, contentType ) {
-    const body = unescape(request( 'get', api, { params: { text, source, target, contentType } } ))
-    if ( contentType === 'html' ) return replace(  body )
+const translate = function translate_translate(api, source = null, target = 'en', text, contentType) {
+    const body = unescape(request('get', api, { params: { text, source, target, contentType } }).responseText)
+    if (contentType === 'html') return replace(body)
     return body
 }
 
